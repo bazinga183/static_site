cc12e39bee79d424ae4bbc7ef56fb7967b07c8dd
bazinga183 <eddie.flores183@gmail.com>
2025-05-21 19:28:48 -0500

    Convert markdown to text nodes

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,5 +1,5 @@
 import unittest
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node, split_nodes_delimiter
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
@@ -7,6 +7,56 @@ class TestTextNode(unittest.TestCase):
         node2 = TextNode("This is a text node", TextType.BOLD)
         self.assertEqual(node, node2)
 
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_split_node(self):
+        node1 = split_nodes_delimiter(
+            [TextNode("This is text with a **bold** word", TextType.TEXT)],
+            "**",
+            TextType.BOLD
+            )
+        node2 = split_nodes_delimiter(
+            [TextNode("This is text with a _italic_ word", TextType.TEXT)],
+            "_",
+            TextType.ITALIC
+            )
+
+        node3 = split_nodes_delimiter(
+            [TextNode("This is text with a `code` word", TextType.TEXT)], 
+            "`",
+            TextType.CODE
+            )
+        node4 = split_nodes_delimiter(
+            [TextNode("This is text with a text word", TextType.TEXT)], 
+            "`",
+            TextType.CODE
+            )
+        test1 = [
+            TextNode("This is text with a ", TextType.TEXT, None),
+            TextNode("bold", TextType.BOLD, None),
+            TextNode(" word", TextType.TEXT, None)
+            ]
+        test2 = [
+            TextNode("This is text with a ", TextType.TEXT, None),
+            TextNode("italic", TextType.ITALIC, None),
+            TextNode(" word", TextType.TEXT, None)
+            ]
+        test3 = [
+            TextNode("This is text with a ", TextType.TEXT, None),
+            TextNode("code", TextType.CODE, None),
+            TextNode(" word", TextType.TEXT, None)
+            ]
+        test4 = [
+            TextNode("This is text with a text word", TextType.TEXT, None)
+            ]
+        self.assertEqual(node1, test1)
+        self.assertEqual(node2, test2)
+        self.assertEqual(node3, test3)
+        self.assertEqual(node4, test4)
 
 if __name__ == "__main__":
     unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from htmlnode import HTMLNode, ParentNode, LeafNode
 
 class TextType(Enum):
     TEXT = "text"
@@ -23,3 +24,44 @@ class TextNode:
     
     def __repr__(self):
         return f'TextNode({self.text}, {self.text_type}, {self.url})'
+    
+def text_node_to_html_node(text_node):
+    if text_node.text_type not in TextType:
+        raise Exception("Text type not valid")
+    elif text_node.text_type == TextType.TEXT:
+        return LeafNode(None, text_node.text)
+    elif text_node.text_type == TextType.BOLD:
+        return LeafNode('b', text_node.text)
+    elif text_node.text_type == TextType.ITALIC:
+        return LeafNode('i', text_node.text)
+    elif text_node.text_type == TextType.CODE:
+        return LeafNode('code', text_node.text)
+    elif text_node.text_type == TextType.LINK:
+        return LeafNode('a', text_node.text, {"href": text_node.url})
+    elif text_node.text_type == TextType.IMAGE:
+        return LeafNode('img', '', {"src": text_node.url, 'alt': text_node.text})
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            new_nodes.append(old_node)
+            continue
+        split_nodes = []
+        sections = old_node.text.split(delimiter)
+        if len(sections) % 2 == 0:
+            raise ValueError("invalid markdown, formatted section not closed")
+        for i in range(len(sections)):
+            if sections[i] == "":
+                continue
+            if i % 2 == 0:
+                split_nodes.append(TextNode(sections[i], TextType.TEXT))
+            else:
+                split_nodes.append(TextNode(sections[i], text_type))
+        new_nodes.extend(split_nodes)
+    return new_nodes
+
+node = TextNode("This is text with a `code` word", TextType.TEXT)
+new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+
+print(new_nodes)
